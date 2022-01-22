23fcc22e878681089ec49750166bfc88ce799a88
MikeCAT <mikecat@users.noreply.github.com>
2022-01-22 14:48:51 +0900

    add tweet button

--- hc.js
+++ hc.js
@@ -10,14 +10,82 @@ window.addEventListener("DOMContentLoaded", function() {
 	const num_of_palindromes = document.getElementById("num_of_palindromes");
 	const happiness = document.getElementById("happiness");
 	const elapsed_time = document.getElementById("elapsed_time");
+	const tweet_link = document.getElementById("tweet_link");
 
 	const progress_area = document.getElementById("progress_area");
 	const progress_bar = document.getElementById("progress_bar");
 	const progress_text = document.getElementById("progress_text");
 
+	const thisPageUriWithoutHash = function() {
+		const uri = location.href;
+		const hashPos = uri.indexOf("#");
+		return hashPos >= 0 ? uri.substring(0, hashPos) : uri;
+	};
+
+	const encodeForUri = function(str) {
+		return encodeURIComponent(str)
+			.replace(/!/g, "%21").replace(/\*/g, "%2A").replace(/'/g, "%27")
+			.replace(/\(/g, "%28").replace(/\)/g, "%29").replace(/%20/g, "+");
+	};
+	tweet_link.href = "https://twitter.com/intent/tweet?text=" + encodeForUri(document.title) +
+		"&url=" + encodeForUri(thisPageUriWithoutHash());
+
 	const setEnableForm = function(enable) {
 		target_str.disabled = !enable;
 		calc_button.disabled = !enable;
+	};
+
+	const getTweetLength = function(str) {
+		let cnt = 0;
+		for (let i = 0; i < str.length; i++) {
+			let c = str.charCodeAt(i);
+			if (c < 0x7f) {
+				cnt++;
+			} else if (i + 1 < str.length && 0xd800 <= c && c <= 0xdbff && 0xdc00 <= str.charCodeAt(i + 1) && str.charCodeAt(i + 1) <= 0xdfff) {
+				cnt += 2;
+				i++;
+			} else {
+				cnt += 2;
+			}
+		}
+		return cnt;
+	};
+
+	const isPairForTweet = function(c1, c2) {
+		const cc1 = c1.charCodeAt(0), cc2 = c2.charCodeAt(0);
+		if ((c1 === "#" || c1 === "@") && c2 == "\u200b") return true;
+		if (0xd800 <= cc1 && cc1 <= 0xdbff && 0xdc00 <= cc2 && cc2 <= 0xdfff) return true;
+		return false;
+	};
+
+	const getTweetUri = function(str, happiness) {
+		const header = "「", middle = "」の幸いさは", footer = "でした。", tag = "幸いさ計算機";
+		const omitIndicator = "…";
+		const uri = thisPageUriWithoutHash() + "#" + encodeForUri(str);
+		const tweetLimit = 280, uriCost = 23;
+		const afterInput = middle + happiness + footer;
+		const nonInputCost = getTweetLength(header + afterInput + " #" + tag) + uriCost + 1;
+		let strToTweet = str.replace(/#/g, "#\u200b").replace(/@/g, "@\u200b");
+		if (getTweetLength(strToTweet) > tweetLimit - nonInputCost) {
+			let badget = tweetLimit - nonInputCost;
+			let left = 0, right = strToTweet.length;
+			let newStrToTweet, newStrToTweetCandidate;
+			for (;;) {
+				left++;
+				if (isPairForTweet(strToTweet.charAt(left - 1), strToTweet.charAt(left))) left++;
+				newStrToTweetCandidate = strToTweet.substring(0, left) + omitIndicator + strToTweet.substring(right);
+				if (getTweetLength(newStrToTweetCandidate) > badget) break;
+				newStrToTweet = newStrToTweetCandidate;
+				right--;
+				if (isPairForTweet(strToTweet.charAt(right - 1), strToTweet.charAt(right))) right--;
+				newStrToTweetCandidate = strToTweet.substring(0, left) + omitIndicator + strToTweet.substring(right);
+				if (getTweetLength(newStrToTweetCandidate) > badget) break;
+				newStrToTweet = newStrToTweetCandidate;
+			}
+			strToTweet = newStrToTweet;
+		}
+		return "https://twitter.com/intent/tweet?text=" + encodeForUri(header + strToTweet + afterInput) +
+		"&url=" + encodeForUri(uri) + "&hashtags=" + encodeForUri(tag);
 	};
 
 	const renderResult = function(len, pnum, division, startTime) {
@@ -43,6 +111,7 @@ window.addEventListener("DOMContentLoaded", function() {
 			result_detail.removeChild(result_detail.firstChild);
 		}
 		result_detail.appendChild(d);
+		tweet_link.href = getTweetUri(division.join(""), happinessValue);
 	};
 
 	const worker = new Worker("hc_worker.js");
